5adeea03989fb18c63b7b4a2ebe27c99fc49ad6c
jortdus <jortdus@gmail.com>
2021-02-18 16:37:44 +0100

    project structure update

---
+++ index.js
@@ -0,0 +1,19 @@
+const dotenv = require('dotenv').config()
+const SteamAPI = require('steamapi');
+const prompt = require('prompt-sync')();
+const steam = new SteamAPI(process.env.STEAMAPI);
+
+
+const name = prompt('What is your steamcommunity url:  ');
+
+steam.resolve(`${name}`).then(id => {
+
+    console.log(id);
+    steam.getUserRecentGames(id).then(recentGames => {
+        for (var i = 0; i < recentGames.length; ++i) {
+            console.log(Math.floor(recentGames[i].playTime / 60));
+        }
+        console.log(recentGames);
+        
+    });
+});
