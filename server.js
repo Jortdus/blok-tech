332081995077f3823e1d33243ff0407dcb2dbbc2
Jort Broekhof <jortdus@gmail.com>
2021-02-22 15:24:38 +0100

    back-end / front-end homework etc

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ app.use('/static', express.static(__dirname + '/static'));
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index');
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
     res.render('about');
 });
 
-app.listen(port, () => {
-  console.log(`listening to port ${port}`)
-});
-
 app.use(function (req, res, next) {
     res.status(404).send("This page does not exist.")
-  })
+})
+
+app.listen(port, () => {
+    console.log(`listening to port ${port}`)
+});
