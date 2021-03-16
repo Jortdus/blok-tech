2c1e6b04efae19088dbb49b5d035fa1c8f3b4f9a
jortdus <jortdus@gmail.com>
2021-03-16 18:17:36 +0100

    MVC work

---
+++ route/steamFetch.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+const express = require('express')
+const router = express.Router()
+const User = require("../model/user.js")
+const SteamAPI = require('steamapi');
+const steam = new SteamAPI(process.env.STEAMAPI);
+
+router.get('/', (req, res) => {
+    const { error } = req.query
+    if (error && error === 'No match') {
+        res.render('layouts/index', { message: 'User not found' })
+        return
+    }
+    res.render('layouts/index', { message: '' })
+})
+
+router.post('/login', (req, res) => {
+    const { id: steamUser } = req.body
+    User.findOne({username: steamUser}).then(result => {
+        if (!result) {
+            steam.resolve(steamUser).then(id => {
+                steam.getUserSummary(id).then(userSummary => {
+                    new User({
+                        username: steamUser,
+                        steamID: userSummary.steamID,
+                        country: userSummary.countryCode || '',
+                        realName: userSummary.realName || ''
+                    })
+                    .save()
+                    .then(result => 
+                        res.redirect('/app')
+                    )
+                })
+            }).catch(err => {
+                res.redirect('/?error=' + err.message)
+            })
+            return
+        }
+        res.redirect('/profile')
+    })
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,62 +4,35 @@ const bodyParser = require('body-parser')
 const app = express();
 const port = process.env.PORT || 5000
 const connectDB = require('./model/db.js')
-const SteamAPI = require('steamapi');
-const steam = new SteamAPI(process.env.STEAMAPI);
+// const SteamAPI = require('steamapi');
+// const steam = new SteamAPI(process.env.STEAMAPI);
 const User = require("./model/user.js")
+const router = require("./route/steamFetch.js")
 
 // call on db connection module
 connectDB();
 
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+// sets views folder
+app.set('views', __dirname + '/views')
 
 // static directory designation 
-app.use('/static', express.static(__dirname + '/static'));
+app.use('/static', express.static(__dirname + '/static'))
 
 // view engine designation 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 // rendering
-app.get('/', (req, res) => {
-    const { error } = req.query
-    if (error && error === 'No match') {
-        res.render('layouts/index', { message: 'User not found' });
-        return
-    }
-    res.render('layouts/index', { message: '' });
-});
-
-app.post('/login', (req, res) => {
-    const { id: steamUser } = req.body
-    User.findOne({username: steamUser}).then(result => {
-        if (!result) {
-            steam.resolve(steamUser).then(id => {
-                steam.getUserSummary(id).then(userSummary => {
-                    new User({
-                        username: steamUser,
-                        steamID: userSummary.steamID,
-                        country: userSummary.countryCode || '',
-                        realName: userSummary.realName || ''
-                    })
-                    .save()
-                    .then(result => 
-                        res.redirect('/app')
-                    )
-                });
-            }).catch(err => {
-                res.redirect('/?error=' + err.message)
-            });
-            return
-        }
-        res.redirect('/profile')
-    })
-})
+app.use(router)
 
 // 404 error handling 
 app.use(function (req, res, next) {
     res.status(404).send("This page does not exist.")
 })
 
-app.listen(port, () => {console.log('Server is running!')});
-
-
+app.listen(port, () => {
+    console.log('Server is running!', port)
+})
